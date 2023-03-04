1eb74bdb963bdf65ce17b424e17f3078a35001de
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-03-04 19:54:35 +0000

    feat: implement `ServerMain`'s `main()` method
    Close #1

---
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/AdminServiceImpl.java
@@ -0,0 +1,7 @@
+package pt.tecnico.distledger.server;
+
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminServiceGrpc;
+
+public class AdminServiceImpl extends AdminServiceGrpc.AdminServiceImplBase {
+
+}
--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
@@ -1,10 +1,43 @@
 package pt.tecnico.distledger.server;
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 public class ServerMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// Receive and print arguments.
+		System.out.printf("Received %d arguments%n", args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
 
-		/* TODO */
+		// Check arguments.
+		if (args.length < 1) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s port qualifier%n", ServerMain.class.getName());
+			return;
+		}
 
+		// Get arguments.
+		final int port = Integer.parseInt(args[0]);
+		final String qualifer = args[1];
+
+		// Initialize services.
+		final BindableService admin = new AdminServiceImpl();
+		final BindableService user = new UserServiceImpl();
+
+		// Create a new server to listen on port.
+		Server server = ServerBuilder.forPort(port).addService(admin).addService(user).build();
+
+		// Start the server
+		server.start();
+
+		// Do not exit the main thread. Wait until server is terminated.
+		server.awaitTermination();
 	}
 
 }
