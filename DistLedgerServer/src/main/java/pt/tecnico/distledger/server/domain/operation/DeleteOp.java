8d5ccc295e4e65886293ba4ae42d49d331f59be1
Gonçalo Nunes <goncaloinunes2002@gmail.com>
2023-03-05 17:03:47 +0000

    feat: implement `ServerState` logic
    Close #2

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/operation/DeleteOp.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/operation/DeleteOp.java
@@ -1,9 +1,16 @@
 package pt.tecnico.distledger.server.domain.operation;
 
+import pt.tecnico.distledger.server.domain.ServerState;
+
 public class DeleteOp extends Operation {
 
 	public DeleteOp(String account) {
 		super(account);
+	}
+
+	@Override
+	public void execute(ServerState state) {
+		state.addDeleteOperation(this);
 	}
 
 }
