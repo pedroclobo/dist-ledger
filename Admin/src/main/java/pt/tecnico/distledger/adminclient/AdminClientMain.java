70d2757657dc31553b04a2ea0ba9ee63b73edc3b
André Torres <andre.torres@tecnico.ulisboa.pt>
2023-03-08 16:11:04 +0000

    feat: implement debug option for admin client

--- Admin/src/main/java/pt/tecnico/distledger/adminclient/AdminClientMain.java
+++ Admin/src/main/java/pt/tecnico/distledger/adminclient/AdminClientMain.java
@@ -3,14 +3,25 @@ package pt.tecnico.distledger.adminclient;
 import pt.tecnico.distledger.adminclient.grpc.AdminService;
 
 public class AdminClientMain {
+
+	/** Set flag to true to print debug messages. 
+	 * The flag can be set using the -Ddebug command line option. */
+	private static final boolean DEBUG_FLAG = (System.getProperty("debug") != null);
+
+	/** Helper method to print debug messages. */
+	private static void debug(String debugMessage) {
+		if (DEBUG_FLAG)
+			System.err.println(debugMessage);
+	}
+
 	public static void main(String[] args) {
 
-		System.out.println(AdminClientMain.class.getSimpleName());
+		debug(String.format("%s", AdminClientMain.class.getSimpleName()));
 
 		// receive and print arguments
-		System.out.printf("Received %d arguments%n", args.length);
+		debug(String.format("Received %d arguments", args.length));
 		for (int i = 0; i < args.length; i++) {
-			System.out.printf("arg[%d] = %s%n", i, args[i]);
+			debug(String.format("arg[%d] = %s", i, args[i]));
 		}
 
 		// check arguments
@@ -22,9 +33,14 @@ public class AdminClientMain {
 
 		final String host = args[0];
 		final int port = Integer.parseInt(args[1]);
+		debug(String.format("host=%s : port=%d", host, port));
 
+		debug("Create CommandParser and AdminService");
 		AdminService adminService = new AdminService(host, port);
 		CommandParser parser = new CommandParser(adminService);
+
+		System.err.println("Hello Administrator");
+		debug("Call parseInput()");
 		parser.parseInput();
 
 		adminService.shutdown();
