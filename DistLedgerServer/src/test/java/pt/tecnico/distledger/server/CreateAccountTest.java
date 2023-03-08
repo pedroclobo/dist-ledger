d61bc70d58da7e5dbafd5dbad62347be96390e21
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-03-08 18:46:23 +0000

    test: add test for basic `ServerState` operations

---
+++ DistLedgerServer/src/test/java/pt/tecnico/distledger/server/CreateAccountTest.java
@@ -0,0 +1,59 @@
+package pt.tecnico.distledger.server;
+
+import pt.tecnico.distledger.server.domain.ServerState;
+import pt.tecnico.distledger.server.domain.operation.CreateOp;
+import pt.tecnico.distledger.server.exceptions.AccountAlreadyExistsException;
+import pt.tecnico.distledger.server.exceptions.CreateBrokerException;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateAccountTest {
+	private static ServerState state;
+
+    @BeforeEach
+    public void setUp() {
+		state = new ServerState();
+    }
+
+    @AfterEach
+    public void tearDown() {
+		state = null;
+    }
+
+	// Create new account
+	@Test
+	public void createAccount() {
+		String account = "Alice";
+		CreateOp op = new CreateOp(account);
+		state.addCreateOperation(op);
+
+		assertEquals(state.getLedger().size(), 1);
+		assertEquals(state.getLedger().get(0), op);
+		assertEquals(state.getAccountBalance(account), 0);
+	}
+
+	// Create the same account twice
+	@Test
+	public void createAccountTwice() {
+		CreateOp op1 = new CreateOp("Alice");
+		CreateOp op2 = new CreateOp("Alice");
+
+		state.addCreateOperation(op1);
+
+		// An exception should be thrown when creating a duplicate account
+		assertThrows(AccountAlreadyExistsException.class, () -> state.addCreateOperation(op2));
+		assertEquals(state.getLedger().size(), 1);
+		assertEquals(state.getLedger().get(0), op1);
+	}
+
+	// Create an account with the name 'broker'
+	@Test
+	public void createBrokerAccount() {
+		CreateOp op = new CreateOp("broker");
+
+		// An exception should be thrown when creating the 'broker' account
+		assertThrows(CreateBrokerException.class, () -> state.addCreateOperation(op));
+		assertEquals(state.getLedger().size(), 0);
+	}
+}
---
+++ DistLedgerServer/src/test/java/pt/tecnico/distledger/server/CreateServerStateTest.java
@@ -0,0 +1,26 @@
+package pt.tecnico.distledger.server;
+
+import pt.tecnico.distledger.server.domain.ServerState;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateServerStateTest {
+	private static ServerState state;
+
+    @BeforeEach
+    public void setUp() {
+		state = new ServerState();
+    }
+
+    @AfterEach
+    public void tearDown() {
+		state = null;
+    }
+
+    @Test
+    public void initState() {
+		assertEquals(state.getServerMode(), ServerState.ServerMode.ACTIVE);
+		assertEquals(state.getLedger().size(), 0);
+    }
+}
