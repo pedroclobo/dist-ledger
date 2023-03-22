0f13f3f3ce44a28f596c730726d49a552c89309b
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-03-22 21:17:04 +0000

    feat: reuse stubs in `DistLedgerServer`

---
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/grpc/StubHandler.java
@@ -0,0 +1,31 @@
+package pt.tecnico.distledger.server.grpc;
+
+import pt.tecnico.distledger.server.domain.operation.*;
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions;
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.LedgerState;
+import pt.ulisboa.tecnico.distledger.contract.distledgerserver.DistLedgerCrossServerServiceGrpc;
+import pt.ulisboa.tecnico.distledger.contract.distledgerserver.CrossServerDistLedger.*;
+
+import java.util.Map;
+
+public class StubHandler {
+	private NamingServerService namingServerService;
+	private Map<String, DistLedgerCrossServerServiceStubHandler> stubHandlers;
+
+	public StubHandler(NamingServerService namingServerService) {
+		this.namingServerService = namingServerService;
+		this.stubHandlers = namingServerService.getHandlers();
+	}
+
+	public DistLedgerCrossServerServiceGrpc.DistLedgerCrossServerServiceBlockingStub getStub(String qualifier) {
+		if (!stubHandlers.containsKey(qualifier)) {
+			stubHandlers.put(qualifier, namingServerService.getHandler(qualifier));
+		}
+
+		return stubHandlers.get(qualifier).getStub();
+	}
+
+	public void shutdown() {
+		stubHandlers.values().forEach(DistLedgerCrossServerServiceStubHandler::shutdown);
+	}
+}
