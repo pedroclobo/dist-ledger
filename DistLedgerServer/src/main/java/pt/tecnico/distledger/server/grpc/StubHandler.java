821a04513ca8829578b45fff1a8cf75b113a8d15
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-03-23 14:27:15 +0000

    feat(DistLedger): rebuild channel if it becomes invalid

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/grpc/StubHandler.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/grpc/StubHandler.java
@@ -6,6 +6,9 @@ import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.Ledger
 import pt.ulisboa.tecnico.distledger.contract.distledgerserver.DistLedgerCrossServerServiceGrpc;
 import pt.ulisboa.tecnico.distledger.contract.distledgerserver.CrossServerDistLedger.*;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ConnectivityState;
+
 import java.util.Map;
 
 public class StubHandler {
@@ -17,11 +20,43 @@ public class StubHandler {
 		this.stubHandlers = namingServerService.getHandlers();
 	}
 
+	private boolean shouldReplaceChannel(String qualifier) {
+		if (!stubHandlers.containsKey(qualifier)) {
+			return true;
+		}
+
+		// This is done twice to refresh the channel state.
+		ConnectivityState state = stubHandlers.get(qualifier)
+		                                      .getChannel()
+		                                      .getState(true);
+		state = stubHandlers.get(qualifier)
+		                    .getChannel()
+		                    .getState(true);
+
+		return state == ConnectivityState.TRANSIENT_FAILURE
+		    || state == ConnectivityState.CONNECTING
+		    || state == ConnectivityState.SHUTDOWN;
+	}
+
+	private void replaceChannel(String qualifier) {
+		if (!stubHandlers.containsKey(qualifier)) {
+			return;
+		}
+
+		stubHandlers.get(qualifier)
+		            .shutdown();
+		stubHandlers.replace(qualifier,
+		    namingServerService.getHandler(qualifier));
+	}
+
 	public DistLedgerCrossServerServiceGrpc.DistLedgerCrossServerServiceBlockingStub getStub(
 	    String qualifier) {
 		if (!stubHandlers.containsKey(qualifier)) {
 			stubHandlers.put(qualifier,
 			    namingServerService.getHandler(qualifier));
+
+		} else if (shouldReplaceChannel(qualifier)) {
+			replaceChannel(qualifier);
 		}
 
 		return stubHandlers.get(qualifier)
