db1ace0539cc9c3d091cecbd3c5153f64f3bedda
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-03-24 16:41:11 +0000

    refactor: remove unused exception

--- SharedUtils/src/main/java/pt/tecnico/distledger/sharedutils/exceptions/ServerAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package pt.tecnico.distledger.namingserver.exceptions;
-
-public class ServerAlreadyExistsException extends RuntimeException {
-	public ServerAlreadyExistsException(String host, int port) {
-		super("ERROR: Server with host " + host + "and port " + port + "already exists.");
-	}
-}
